bdce6c85c2c6cce39c41976913f1773454858da9
jbg <joaobenine@gmail.com>
2023-11-20 12:59:11 -0300

    init header

---
+++ src/app/components/partials/Header.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from '../../styles/header.module.css'
+
+export function Header() {
+  return (
+      <header className={styles.header}>
+      <div className="containerMain">
+          <Image src="/assets/logo.png" alt="Logo Delivery" width={158} height={48}  />
+      </div>
+    </header>
+  )
+}
