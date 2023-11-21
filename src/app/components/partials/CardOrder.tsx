b772a3abf22da6e52eb315241488bb8dc0bb113d
jbg <joaobenine@gmail.com>
2023-11-21 16:00:57 -0300

    consumindo dados restantes para formação do pedido

--- src/app/components/partials/CardOrder.tsx
+++ src/app/components/partials/CardOrder.tsx
@@ -5,14 +5,18 @@ import Image from "next/image";
 import { useDataContext } from "@/app/data/hooks/useContext";
 
 export function CardOrder() {
-  const [sizeSelect, setSizeSelect] = useState<string>();
-  const { pedidosData } = useDataContext();
+  const [sizeSelect, setSizeSelect] = useState<number>();
+  const { pedidosData, setSizeValueOrder } = useDataContext();
   // const pedidosSize = pedidosData?sizes[0];
 
   function handleSize(event: React.ChangeEvent<HTMLInputElement>) {
     const valueSelect = event.target.value;
-    setSizeSelect(valueSelect);
+    const numericValue = parseFloat(valueSelect);
+    console.log(numericValue)
+    setSizeSelect(numericValue);
   }
+
+  // function onSubmitSize() {}
 
   return (
     <div className={styles.card}>
@@ -44,17 +48,16 @@ export function CardOrder() {
             rechear do seu jeito! Obs: não trocamos nem adicionamos itens a esse
             copo!
           </p>
-          <div className={styles.selectionsIntro}>
-            <span>
-              <h4>Escolha o tamanho</h4>
-              <p>Escolha pelo menos 1 opção.</p>
-            </span>
-            <p>1/3</p>
-          </div>
-          <form className={styles.selectionsForm}>
-            {pedidosData?.sizes ? (
-              <>
-                <h2>Tamanhos:</h2>
+          {pedidosData?.sizes ? (
+            <>
+              <div className={styles.selectionsIntro}>
+                <span>
+                  <h4>Escolha o tamanho</h4>
+                  <p>Escolha pelo menos 1 opção.</p>
+                </span>
+                <p>1/3</p>
+              </div>
+              <form className={styles.selectionsForm}>
                 {pedidosData.sizes.options.map((size, index) => (
                   <label key={index} htmlFor={size}>
                     <p>{size}</p>
@@ -70,11 +73,71 @@ export function CardOrder() {
                     </span>
                   </label>
                 ))}
-              </>
-            ) : (
-              <p>Carregando dados...</p>
-            )}
-          </form>
+              </form>
+            </>
+          ) : (
+            <p>Carregando dados...</p>
+          )}
+          {pedidosData?.fruits ? (
+            <>
+              <div className={styles.selectionsIntro}>
+                <span>
+                  <h4>Escolha as frutas</h4>
+                  <p>Escolha pelo menos 1 opção.</p>
+                </span>
+                <p>2/3</p>
+              </div>
+              <form className={styles.selectionsForm}>
+                {pedidosData.fruits.options.map((fruit, index) => (
+                  <label key={index} htmlFor={fruit}>
+                    <p>{fruit}</p>
+                    <span>
+                      +R${pedidosData.fruits.prices[index]}
+                      <input
+                        type="radio"
+                        name={`fruit_${index}`}
+                        id={fruit}
+                        value={pedidosData.fruits.prices[index]}
+                        // onChange={(event) => handleSize(event)}
+                      />
+                    </span>
+                  </label>
+                ))}
+              </form>
+            </>
+          ) : (
+            <p>Carregando dados...</p>
+          )}
+          {pedidosData?.complements ? (
+            <>
+              <div className={styles.selectionsIntro}>
+                <span>
+                  <h4>Escolha os complementos</h4>
+                  <p>Escolha pelo menos 1 opção.</p>
+                </span>
+                <p>3/3</p>
+              </div>
+              <form className={styles.selectionsForm}>
+                {pedidosData.complements.options.map((complement, index) => (
+                  <label key={index} htmlFor={complement}>
+                    <p>{complement}</p>
+                    <span>
+                      +R${pedidosData.complements.prices[index]}
+                      <input
+                        type="radio"
+                        name={`complement_${index}`}
+                        id={complement}
+                        value={pedidosData.complements.prices[index]}
+                        // onChange={(event) => handleSize(event)}
+                      />
+                    </span>
+                  </label>
+                ))}
+              </form>
+            </>
+          ) : (
+            <p>Carregando dados...</p>
+          )}
         </article>
       </div>
       <div className={styles.actions}>
